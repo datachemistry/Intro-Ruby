4dd41747e3da20c244b3039e548277ba3c22dbd3
datachemistry <biorrefinaria@gmail.com>
2021-02-06 15:03:00 -0300

    Curso-Finalizado 06-02-2121

---
+++ Ruby-Alura-Pt1/Adivinhacao.rb
@@ -0,0 +1,126 @@
+def dar_boas_vindas
+    puts
+    puts "        P  /_\\  P                              "
+    puts "       /_\\_|_|_/_\\                             "
+    puts "   n_n | ||. .|| | n_n         Bem vindo ao    "
+    puts "   |_|_|nnnn nnnn|_|_|     Jogo de Adivinhação!"
+    puts "  |' '  |  |_|  |'  ' |                        "
+    puts "  |_____| ' _ ' |_____|                        " 
+    puts "        \\__|_|__/                              "
+    puts
+    puts "Qual é o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, #{nome}"
+    nome
+end 
+
+def pede_dificuldade
+    puts "Qual nível de dificuldade que deseja? (1) Fácil, (2)Médio, (3) Difícil"
+    puts "Escolha:"
+    dificuldade = gets.to_i
+end
+
+def sorteia_numero_secreto(dificuldade)
+    case dificuldade
+    when 1
+        maximo = 100
+    when 2
+        maximo = 150
+    when 3
+        maximo = 200
+    end
+    puts "Escolhendo um número secreto entre 1 e #{maximo}..."
+    sorteado = rand(maximo) + 1
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+    sorteado
+end
+
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
+    puts "Tentativa #{tentativa} de #{limite_de_tentativas}"
+    puts "Chutes até agora #{chutes}"
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou #{chute} "
+    chute.to_i
+end
+
+
+def ganhou
+    puts
+    puts "             OOOOOOOOOOO               "
+    puts "         OOOOOOOOOOOOOOOOOOO           "
+    puts "      OOOOOO  OOOOOOOOO  OOOOOO        "
+    puts "    OOOOOO      OOOOO      OOOOOO      "
+    puts "  OOOOOOOO  #   OOOOO  #   OOOOOOOO    "
+    puts " OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  "
+    puts "OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  "
+    puts " OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   "
+    puts "  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    "
+    puts "    OOOOO   OOOOOOOOOOOOOOO   OOOO     "
+    puts "      OOOOOO   OOOOOOOOO   OOOOOO      "
+    puts "         OOOOOO         OOOOOO         "
+    puts "             OOOOOOOOOOOO              "
+    puts
+    puts "               Acertou!                "
+    puts
+end
+
+
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
+    
+    if acertou
+        ganhou
+        return true
+    end
+
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end
+    false
+end
+
+
+def joga (nome, dificuldade)
+    numero_secreto = sorteia_numero_secreto(dificuldade)
+    pontos_ate_agora = 1000
+    limite_de_tentativas = 5
+    chutes = []
+
+    for tentativa in 1..limite_de_tentativas 
+        chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+        chutes << chute
+
+        pontos_a_perder = (chute - numero_secreto).abs/2
+        pontos_ate_agora -= pontos_a_perder
+    
+        if verifica_se_acertou numero_secreto, chute
+            break
+        end
+    end
+
+    puts "Você ganhou #{pontos_ate_agora} pontos"
+end
+
+nome = dar_boas_vindas
+dificuldade = pede_dificuldade
+
+def quer_jogar?
+    puts "Deseja jogar novamente? (S/N)"
+    quero_jogar = gets.strip
+    quero_jogar.upcase == "S"
+end
+
+loop do
+    joga nome, dificuldade
+    if not quer_jogar?
+        break
+    end    
+end
